9a56abfe354e4dca1be4d86f024839fe3d1f3bf9
qeesung <1245712564@qq.com>
2017-06-08 00:37:20 +0800

    update test case

--- test/test.js
+++ test/test.js
@@ -118,6 +118,83 @@ describe("Promise cost", () => {
       })
     });
   });
+
+  describe("promise resolve", () => {
+    it("resolve the promise in 1000ms, notify should also work", () => {
+      let notifyMockFn = sinon.spy();
+      return new Promise((resolve, reject) => { // notify function only called here
+        setTimeout(reject, 1000);
+      }, {
+        notifier:{
+          notify: notifyMockFn
+        },
+      }).catch(() => {
+      }).then(() => {
+        notifyMockFn.should.have.been.calledOnce;
+      });
+    });
+
+    it("resolve the promise in 1000ms, and sum the time cost,  notify should multi times", () => {
+      let notifyMockFn = sinon.spy();
+      return new Promise((resolve, reject) => { // notify called here #1
+        setTimeout(resolve, 1000);
+      }, {
+        sum: true,
+        notifier: {
+          notify: notifyMockFn
+        }
+      }).then(() => { // notify called here #2
+      }).then(() => {
+        notifyMockFn.should.have.been.calledTwice;
+      })
+    });
+  });
+
+  describe("Promise cost name", () => {
+    it("when sum option is true, the number at the end of promise name should auto increment", () =>{
+      let notifyMockFn = sinon.spy();
+      let name = "cost";
+      return new Promise(resolve => {
+        setTimeout(resolve, 1000);
+      }, {
+        name: name,
+        sum: true,
+        notifier: {
+          notify: notifyMockFn
+        }
+      }).then(() => {
+        notifyMockFn.should.have.been.calledWith(name);
+      }).then(() => {
+        notifyMockFn.should.have.been.calledWith(`${name}-#1`);
+      }).then(() => {
+        notifyMockFn.should.have.been.calledWith(`${name}-#2`);
+      });
+    });
+
+    it("when sum options is false, the number at the end of promise name should auto increment", function(){
+      this.timeout(5000);
+      let notifyMockFn = sinon.spy();
+      let name = "cost";
+      return new Promise(resolve => {
+        setTimeout(resolve, 1000);
+      }, {
+        name: name,
+        notifier: {
+          notify: notifyMockFn
+        }
+      }).then(() => { // cost-#1, won't call notify
+        notifyMockFn.should.have.been.calledWith(name);
+        return new Promise(resolve => {
+          setTimeout(resolve, 1000);
+        }, {
+          notifier: {notify: () =>{}}
+        });
+      }).then(() => { // cost-#2, cost exceed 1000ms, will be called
+      }).then(() => { // cost-#3, won't call notify
+        notifyMockFn.should.have.been.calledWith(`${name}-#2`);
+      });
+    });
+  });
 });
 
 
